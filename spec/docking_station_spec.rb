c49156f937fadf4901b9e216783915b34cfa9e99
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-06-30 15:51:48 +0100

    add test to release and dock bikes, add basic methods to release and dock bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,8 +3,21 @@ require 'docking_station'
 describe DockingStation do
   it { should respond_to(:release_bike) }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
+
+  describe "#release_bike" do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+  end 
 end
