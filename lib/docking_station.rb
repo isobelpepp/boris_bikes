94889b629b16b9ab215005cbd1bd27054e1445b6
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-06-30 16:59:19 +0100

    Rspec and docking_station account for 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,14 +2,30 @@ require_relative 'bike'
 
 class DockingStation
 
+  def initialize
+    @bikes = []
+  end
+
   attr_reader :bike
 
   def release_bike
-    @bike
+    fail 'No bikes available' if empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    @bike = bike
+    fail 'Station full' if full?
+    @bikes.push(bike)
+  end
+
+  private
+
+  def full?
+    @bikes.count > 19
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,18 +6,23 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:bike) }
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
-
   describe "#release_bike" do
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
-
+ 
+    it 'raises an error when no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end 
+
+  describe "#dock" do
+    it 'raise an error when dock is full' do
+      20.times {subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error 'Station full'
+    end
+  end
+ 
 end
